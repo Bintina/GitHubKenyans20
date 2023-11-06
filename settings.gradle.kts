15dd8110f5cdfc9ae6e14c0fa176385f26f85944
Bintina <105106238+Bintina@users.noreply.github.com>
2023-11-06 11:53:21 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GitHub Kenyans 2.0"
+include(":app")
+ 
